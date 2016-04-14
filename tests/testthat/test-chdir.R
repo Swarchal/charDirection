7bf3401e34da2bfe6ecb15651d24bee5ced90e79
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-04-14 16:13:14 +0100

    more tests for chdir

--- tests/testthat/test-chdir.R
+++ tests/testthat/test-chdir.R
@@ -9,13 +9,18 @@ a_const <- matrix(rnorm(10000), 100, 100)
 a_const[1, ] <- rep(1, 100)
 
 test_that("unequal rows returns error",{
-    expect_error(chdir(a, b)) 	  
+    expect_error(chdir(a, b, samples = 1:100)) 	  
 })
 
 test_that("contains NA returns error",{
-    expect_error(chdir(a, aNA))
+    expect_error(chdir(a, aNA, samples = 1:100))
 })
 
 test_that("constant variance returns error",{
-    expect_error(chdir(a, a_const))
+    expect_error(chdir(a, a_const, samples = 1:100))
+    expect_error(chdir(a_const, a, samples = 1:100))
+})
+
+test_that("runs without error",{
+    expect_is(chdir(a, a2, samples = 1:100), 'matrix')
 })
